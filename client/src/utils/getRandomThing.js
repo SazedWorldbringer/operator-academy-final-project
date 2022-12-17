f815ccef2982807a41d30dfb6b25b68a44f4d27b
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-17 16:14:58 +0530

    fix: fix getOptionsForVote function by passing in max number of values

--- client/src/utils/getRandomThing.js
+++ client/src/utils/getRandomThing.js
@@ -1,13 +1,15 @@
-export const getRandomThing = (notThisOne, maxNumOfThings) => {
+export const getRandomThing = (maxNumOfThings, notThisOne) => {
   const thingNumber = Math.floor(Math.random() * (maxNumOfThings - 1) + 1)
 
   if (thingNumber !== notThisOne) return thingNumber;
   return getRandomThing(notThisOne)
 }
 
-export const getOptionsForVote = () => {
-  const first = getRandomThing();
-  const second = getRandomThing(first);
+export const getOptionsForVote = (things) => {
+  let maxNum = Object.keys(things).length
+
+  const first = getRandomThing(maxNum);
+  const second = getRandomThing(maxNum, first);
 
   return [Object.values(things)[first], Object.values(things)[second]];
 }
