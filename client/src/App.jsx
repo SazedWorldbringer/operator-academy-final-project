eef984cd257ada3f7f4cf53b16bb7641d6415164
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-17 16:15:20 +0530

    feat: render CardsContainer, and cool bg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
+import CardsContainer from './components/CardsContainer'
+
 const App = () => {
+  /*  return (
+      <div className="relative">
+        <div className="opacity-100">
+          <CardsContainer />
+        </div>
+        <div className="absolute top-0 left-32 w-[700px] h-[700px] bg-purple-700 rounded-full  blur-2xl opacity-50 animate-blob"></div>
+        <div className="absolute top-10 right-96 w-[600px] h-[600px] bg-indigo-700 rounded-full  blur-2xl opacity-50 animate-blob animation-delay-2000"></div>
+        <div className="absolute right-[50%] top-[50%] w-[500px] h-[500px] bg-red-600 rounded-full  blur-2xl opacity-50 animate-blob animation-delay-4000"></div>
+      </div>
+    ) */
+
   return (
     <div>
-      <p className="first-letter:text-red-600 text-black bg-blue-300 text-3xl py-6 my-0 mx-auto">Hello</p>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <CardsContainer />
     </div>
   )
 }
