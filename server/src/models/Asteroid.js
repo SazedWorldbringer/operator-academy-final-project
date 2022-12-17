55e78e02007df1a96331bdd77d8c6742b78ac204
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-17 16:42:34 +0530

    feat: write sample asteroid model

--- server/src/models/Asteroid.js
+++ server/src/models/Asteroid.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-// const ObjectId = Schema.ObjectId;
+// const ObjectId = mongoose.Types.ObjectId;
 
 const AsteroidSchema = new Schema({
   title: String,
 });
 
-const AsteroidModel = mongoose.model('Asteroid', AsteroidSchema)
+const AsteroidModel = mongoose.model("Asteroid", AsteroidSchema);
 
-module.exports = { AsteroidModel }
+module.exports = AsteroidModel
