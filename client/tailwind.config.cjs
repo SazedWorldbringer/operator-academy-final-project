18c0006949ca2d19e74b3ff721abb306dc476ae8
Atharva Pardeshi <atharva.exe@gmail.com>
2022-12-17 16:12:45 +0530

    feat: extend animations

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -18,6 +18,34 @@ module.exports = {
       aspectRatio: {
         card: { aspectRatio: '1 / 1.6' },
       },
+      animation: {
+        tilt: "tilt 10s linear infinite",
+        blob: "blob 10s infinite"
+      },
+      keyframes: {
+        tilt: {
+          "0%, 50%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "25%": {
+            transform: "rotate(10deg)",
+          },
+          "75%": {
+            transform: "rotate(-10deg)",
+          },
+        },
+        blob: {
+          "0%, 100%": {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          "33%": {
+            transform: 'translate(100px, -70px) scale(1.1)',
+          },
+          "66%": {
+            transform: 'translate(20px -20px) scale(0.9)',
+          },
+        },
+      },
     },
   },
   plugins: [],
