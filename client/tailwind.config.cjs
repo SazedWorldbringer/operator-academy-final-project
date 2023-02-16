b1375d62639cf80e6eaae37e3ca7686589a19dd6
Atharva Pardeshi <atharva.exe@gmail.com>
2023-02-16 11:17:32 +0530

    fix: remove theme extension, bring index.css back to base

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,48 +5,7 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundPosition: {
-        card: ''
-      },
-      backgroundSize: {
-        '400': '400%'
-      },
-      backgroundColor: {
-        'container': 'rgb(10, 10, 10)',
-      },
-      aspectRatio: {
-        card: { aspectRatio: '1 / 1.6' },
-      },
-      animation: {
-        tilt: "tilt 10s linear infinite",
-        blob: "blob 10s infinite"
-      },
-      keyframes: {
-        tilt: {
-          "0%, 50%, 100%": {
-            transform: "rotate(0deg)",
-          },
-          "25%": {
-            transform: "rotate(10deg)",
-          },
-          "75%": {
-            transform: "rotate(-10deg)",
-          },
-        },
-        blob: {
-          "0%, 100%": {
-            transform: 'translate(0px, 0px) scale(1)',
-          },
-          "33%": {
-            transform: 'translate(100px, -70px) scale(1.1)',
-          },
-          "66%": {
-            transform: 'translate(20px -20px) scale(0.9)',
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
